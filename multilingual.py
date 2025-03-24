c05297c60cb51d277494c895f1b3d48e89abfedf
Sudeshna-Merugu <sudeshnm@andrew.cmu.edu>
2025-03-24 15:40:14 -0400

    Update multilingual.py

--- multilingual.py
+++ multilingual.py
@@ -1,13 +1,8 @@
 import torch
 from TTS.utils.radam import RAdam
 import torch.serialization
-
-# Add RAdam to the safe globals list
-torch.serialization.add_safe_globals([RAdam])
-
-# Now import TTS
 from TTS.api import TTS
 
-# For German
+torch.serialization.add_safe_globals([RAdam])
 tts = TTS("tts_models/de/thorsten/tacotron2-DDC")
 tts.tts_to_file(text="Ich bin eine Testnachricht.", file_path="german_tts.wav")
